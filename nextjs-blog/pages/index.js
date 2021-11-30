df7993658a2682974c8791543033952f2e161469
kynel <kynel95@gmail.com>
2021-11-30 16:42:36 +0900

    Polishing page

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
+import Date from 'components/date';
 import Layout, { siteTitle } from 'components/layout';
+
 import utilStyles from 'styles/utils.module.css';
 import { getSortedPostsData } from 'lib/posts';
 
@@ -20,12 +24,14 @@ const Home = ({ allPostsData }) => {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.lisg}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItems} key={id}>
-              {title}
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
